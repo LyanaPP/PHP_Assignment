c9df3a665ed4559856ac9f723e6cb2eaf923af5c
pengpengyang <lyana.q669@gmail.com>
2024-02-06 09:19:30 +0200

    modify

--- PHP_24/src/PHP_tasks/crudAPP/db.php
+++ PHP_24/src/PHP_tasks/crudAPP/db.php
@@ -1,7 +1,4 @@
-<?php
-$title = "Exercise 7: CURD-Create";
-$md="Please use appropriate meta Description";
-include '../header.php'; ?>
+
 
 
 
@@ -25,9 +22,7 @@ $conn = new mysqli($servername,$username,$password,$dbname);
 if ($conn->connect_error){
     die("Vonnection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+//echo "Connected successfully";
 
 //$conn->close();
 ?>
-
-<?php include '../footer.php'; ?>
